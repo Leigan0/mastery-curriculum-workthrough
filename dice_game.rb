0c1ba31c76903f502cd032579c2ca89ae4b632f6
Leigh-ann <leighann.gant@gmail.com>
2017-11-24 21:18:49 +0000

    Dice game upgraded - best of three and two dice

--- dice_game.rb
+++ dice_game.rb
@@ -2,17 +2,19 @@ def dice_game
 
 game_count = 0
 
-  while game_count < 1
+
+  while game_count < 3
     player1 = rand(1..6) * 2
     player2 = rand(1..6) * 2
     game_count += 1
-      if player1 > player2
-        puts 'I win'
-        puts pl
-      elsif player1 == player2
-        puts 'Draw'
-      else
-        puts 'I lose'
+      if game_count == 3
+        if player1 > player2
+          puts 'I win'
+        elsif player1 == player2
+          puts 'Draw'
+        else
+          puts 'I lose'
+        end
       end
     end
 end
