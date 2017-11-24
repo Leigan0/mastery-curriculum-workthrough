71bdb4aed67d155e9628ec60e02492d022f8c6c5
Leigh-ann <leighann.gant@gmail.com>
2017-11-24 21:11:08 +0000

    game upgraded so both players roll two dice

--- dice_game.rb
+++ dice_game.rb
@@ -3,11 +3,12 @@ def dice_game
 game_count = 0
 
   while game_count < 1
-    player1 = rand(1..6)
-    player2 = rand(1..6)
+    player1 = rand(1..6) * 2
+    player2 = rand(1..6) * 2
     game_count += 1
       if player1 > player2
         puts 'I win'
+        puts pl
       elsif player1 == player2
         puts 'Draw'
       else
