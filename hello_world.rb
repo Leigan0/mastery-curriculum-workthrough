466f2cc9782b9c545496b5aa5ad148bf0971d557
Leigh-ann <leighann.gant@gmail.com>
2017-11-24 19:26:11 +0000

    comparision operators added

--- hello_world.rb
+++ hello_world.rb
@@ -2,8 +2,17 @@ puts 1 + 1
 
 #Modulo
 
-
+puts 1 % 1 #should equal 0
 puts 3 % 3 #should equal 0
 puts 4 % 2 #should equal 0
 puts 5 % 2 #should equal 1
 puts 110 % 20 #should equal 10
+
+#Comparison operators
+
+puts 1 < 2 #true less than
+puts 1 > 2 #false greater than
+puts 1 >= 1 #true greater than of equal to
+puts 5 <= 10 #true less than or equal to
+puts 5 == 5 #true - equal to
+puts 5 != 6 #true - does not equal
