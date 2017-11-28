29c3d226cdf16019e179f54305aae1a9b3b198ec
Leigh-ann <leighann.gant@gmail.com>
2017-11-28 17:08:51 +0000

    Dice game fixed to meet best of three games specification

---
+++ adventure_game.rb
@@ -0,0 +1,57 @@
+#The user starts by facing forward. They can move right, left, or forward. If
+#they move right, they die (there's a goblin). If they move left,
+#they die (there's a werewolf). If they move forward, they live.
+#They have to do this twice to win.
+
+#Specifications
+#The user sees a request asking them to enter the direction they wish to move
+#The user enters their direction
+#If the the user chooses right, they die - the program displays a message
+#explaining there is a goblin
+#If the user chooses left they die - the program displays a message explaining
+#there is a werewolf
+#If the user chooses forward the program displays a message asking them to pick
+# a direction
+#To win the game the user must select forward twice
+
+#Suggested specification
+#
+# The user is told they're facing forward and can type 'forward', 'left', or 'right'.
+# If the user enters 'right', they die (goblin).
+# If the user enters 'left', they die (werewolf).
+# If the user enters 'forward', they're told they live, and can move again.
+# If the user enters anything other than 'right', 'left', or 'forward', they just get the message from 1 again.
+# If the user enters 'right', they die (goblin).
+# If the user enters 'left', they die (werewolf).
+# If the user enters 'forward', they're told they won and the game exits.
+# If the user enters anything other than 'right', 'left', or 'forward', they just get the message from 4 again.
+
+next_move = ""
+forward_count = 0
+alive = true
+
+while alive == true
+
+until next_move == "left" || next_move == "right" || next_move == "forward"
+  puts "You are facing forward, you must choose your next move!"
+  puts "Do you want to go left, right or forward?!?!?"
+  next_move = gets.chomp.downcase
+end
+
+  if next_move == "left"
+    puts "You have been attacked by a Goblin, YOU DIE!!!"
+    alive = false
+  elsif next_move == "right"
+    puts "You have been eaten by Werewolf, YOU DIE!!!"
+    alive = false
+  elsif next_move == "forward" && forward_count == 2
+    puts "You have won!"
+    exit
+  else
+    puts "You live. You are facing forward, you must choose your next move!"
+    puts "Do you want to go left, right or forward?!?!?"
+    forward_count += 1
+    next_move = gets.chomp.downcase
+  end
+
+end
--- dice_game.rb
+++ dice_game.rb
@@ -1,19 +1,27 @@
 def dice_game
 
 game_count = 0
+player1_wins = 0
+player2_wins = 0
 
-  while game_count < 3
-    player1 = rand(1..6) * 2
-    player2 = rand(1..6) * 2
-    game_count += 1
-      if game_count == 3
+  until game_count == 3
+      player1 = rand(1..6) * 2
+      player2 = rand(1..6) * 2
+      puts player1
+      puts player2
+      game_count += 1
         if player1 > player2
-          puts 'I win'
-        elsif player1 == player2
-          puts 'Draw'
-        else
-          puts 'I lose'
+          player1_wins += 1
+        elsif player1 < player2
+          player2_wins += 1
         end
       end
+
+      if player1_wins > player2_wins
+        puts "Player 1 wins"
+      elsif player1_wins < player2_wins
+        puts "Player 2 wins"
+      else
+        puts "Draw"
     end
 end
