c3a0c7ef3cb0176cdcdcb43203c0ba306ec39fae
Leigh-ann <leighann.gant@gmail.com>
2017-12-04 14:47:33 +0000

    Chapter 8 and 9 tasks completed

--- hello_world.rb
+++ hello_world.rb
@@ -86,61 +86,61 @@
 # one_string + 1 # throw an error - cant add different class together - return values are not the same - would need to convert to same class
 
 #chapter 6 - Arrays
-
-array = Array.new
-
-array.push("Hello world")
-array.push(1)
-array.push(1.0)
-array.push("Goodbye")
-
-another_array = ['hi','there','i','am','an','array']
-
-array.delete_at(2) #remove second element from Array
-array.pop #Will remove last element from array
-array[3] #will return 4th element of array
-
-another_array.slice[0,1] #will return hi -
-
-sentence = ["Hello,", "you", "are", "NOT", "welcome", "here"]
-
-sentence.join(" ")
-sentence.delete("NOT")
-
-greeting = "Hello world"
-greeting[0] #return H
-greeting.split #return array ["Hello", "world"]
-greeting.split("l") # return ["He", "", "o Wor", "d!"]
-
-bad_string = "Why|am|I|so|hard|to|read"
-bad_string.split("|").join(" ") # return "Why am I so hard to read"
-
-groups = [
-  ["Mary", "Sam"],
-  ["Peter", "Kay"]
-  ]
-
-team_1 = groups[0] # ["Mary", "Sam"]
-team_2 = groups[1] # ["Peter", "Kay"]
-
-team_1 = ["Mary", "Sam"]
-team_2 = ["Peter", "Kay"]
-
-groups = [team_1, team_2] #[["Mary", "Sam"], ["Peter", "Kay"]]
-
-arr_1 = [1,2,3,4]
-arr_2 = [4,5,6,7]
-arr_1 + arr_2 #[1, 2, 3, 4, 4, 5, 6, 7]
-
-arr_1.length #4
-
-my_array = ["Hello", "there", "friend!"]
-current_index = 0
-
-while current_index < my_array.length do
-  puts "I'm looping!"
-  current_index += 1
-end
+#
+# array = Array.new
+#
+# array.push("Hello world")
+# array.push(1)
+# array.push(1.0)
+# array.push("Goodbye")
+#
+# another_array = ['hi','there','i','am','an','array']
+#
+# array.delete_at(2) #remove second element from Array
+# array.pop #Will remove last element from array
+# array[3] #will return 4th element of array
+#
+# another_array.slice[0,1] #will return hi -
+#
+# sentence = ["Hello,", "you", "are", "NOT", "welcome", "here"]
+#
+# sentence.join(" ")
+# sentence.delete("NOT")
+#
+# greeting = "Hello world"
+# greeting[0] #return H
+# greeting.split #return array ["Hello", "world"]
+# greeting.split("l") # return ["He", "", "o Wor", "d!"]
+#
+# bad_string = "Why|am|I|so|hard|to|read"
+# bad_string.split("|").join(" ") # return "Why am I so hard to read"
+#
+# groups = [
+#   ["Mary", "Sam"],
+#   ["Peter", "Kay"]
+#   ]
+#
+# team_1 = groups[0] # ["Mary", "Sam"]
+# team_2 = groups[1] # ["Peter", "Kay"]
+#
+# team_1 = ["Mary", "Sam"]
+# team_2 = ["Peter", "Kay"]
+#
+# groups = [team_1, team_2] #[["Mary", "Sam"], ["Peter", "Kay"]]
+#
+# arr_1 = [1,2,3,4]
+# arr_2 = [4,5,6,7]
+# arr_1 + arr_2 #[1, 2, 3, 4, 4, 5, 6, 7]
+#
+# arr_1.length #4
+#
+# my_array = ["Hello", "there", "friend!"]
+# current_index = 0
+#
+# while current_index < my_array.length do
+#   puts "I'm looping!"
+#   current_index += 1
+# end
 
 #returns
 # I'm looping!
@@ -149,69 +149,170 @@ end
 
 #altered to use a break
 
-my_array = ["Hello", "there", "friend!"]
-current_index = 0
-
-while true
-  break if current_index > my_array.length
-  puts "I'm looping!"
-  current_index += 1
-end
+# my_array = ["Hello", "there", "friend!"]
+# current_index = 0
+#
+# while true
+#   break if current_index > my_array.length
+#   puts "I'm looping!"
+#   current_index += 1
+# end
 
 #returns
 # I'm looping!
 # I'm looping!
 # I'm looping!
 
-my_array = ["Hello", "there", "friend!"]
-current_index = 0
-
-while current_index < my_array.length do
-  puts my_array[current_index]
-  current_index += 1
-end
-# puts
+# my_array = ["Hello", "there", "friend!"]
+# current_index = 0
+#
+# while current_index < my_array.length do
+#   puts my_array[current_index]
+#   current_index += 1
+# end
+# # puts
 # Hello
 # there
 # friend!
 
+#
+# my_array = ["Hello", "there", "friend!"]
+#
+# my_array.each do
+#   puts "I'm looping!"
+# end
+# #I'm looping!
+# # I'm looping!
+# # I'm looping!
+# #  => ["Hello", "there", "friend!"]
+#
+# my_array = ["Hello", "there", "friend!"]
+#
+# my_array.each do |element|
+#   puts element
+# end
+# #
+# # Hello
+# # there
+# # friend!
+# #  => ["Hello", "there", "friend!"]
+#
+# list_of_numbers = [17, 2, -1, 88, 7]
+# accumulator = []
+#
+# list_of_numbers.each do |number|
+#   if number < 10
+#     accumulator.push(number)
+#   end
+# end
+#
+# accumulator
+#
+# #[2, -1, 7]
+# #list of numbers is not altered in any way
+#
+# words = ["Hello", "World!"]
+#
+# words.include?("Hello")
+#return true
 
-my_array = ["Hello", "there", "friend!"]
+#introducing hashes
+#
+# curse = "dang"
+#
+# # First, set up the options
+# beratings = {
+#   "damn" => "That's a curse word! How dare you",
+#   "dang" => "That's a less bad curse word! Still, how dare you",
+#   "darn" => "Hmm, I'm mildly offended but I'll survive. Watch your language!",
+#   "durn" => "Ahh, that's good Southern swearing, that is!"
+#   }
+#
+# # Second, do a lookup on the beratings hash
+# beratings[curse]
+#
+# my_hash = { :my_array => ["1", "2", "3", "4"] }
+#  #=> {:my_array=>["1", "2", "3", "4"]}
+# my_hash[:my_array].delete_at(0)
+#  #=> "1"
+# my_hash[:my_array].push("5").push("6")
+# # => ["2", "3", "4", "5", "6"]
+# my_hash
+# # => {:my_array=>["2", "3", "4", "5", "6"]}
+#
+# #Group by sport values
+#
+# players = [
+#   { :name => "Sam", :sport => "tennis" },
+#   { :name => "Mary", :sport => "squash" },
+#   { :name => "Ed", :sport => "tennis" },
+#   { :name => "Mark", :sport => "football" }
+#   ]
+#
+#   players.each do |player|
+#     sport = player[:sport]
+#
+#   if players_by_sport[sport] == nil
+#     players_by_sport[sport] = []
+#   end
+#
+#   players_by_sport[sport].push(player[:name])
+# end
+#
+# players_by_sport
 
-my_array.each do
-  puts "I'm looping!"
+ #Methods
+
+ def gimme_ten
+   # We want the return value to be 10
+   return 10
+ end
+  # => 10
+
+  def stop_halfway
+  return "Stop here"
+  sum = 1 + 1
+  return sum
 end
-#I'm looping!
-# I'm looping!
-# I'm looping!
-#  => ["Hello", "there", "friend!"]
+# => Stop here
 
-my_array = ["Hello", "there", "friend!"]
-
-my_array.each do |element|
-  puts element
+def stop_halfway
+  sum = 1 + 1
+  return sum
 end
 #
-# Hello
-# there
-# friend!
-#  => ["Hello", "there", "friend!"]
+# => 2
 
-list_of_numbers = [17, 2, -1, 88, 7]
-accumulator = []
+def stop_halfway
+  sum = 1 + 1
+end
+# => returns sum
 
-list_of_numbers.each do |number|
-  if number < 10
-    accumulator.push(number)
-  end
+def hello(person)
+  return "Hello, " + person + "!"
 end
 
-accumulator
+hello("Leigh")
 
-#[2, -1, 7]
-#list of numbers is not altered in any way
+#=> Hello Leigh!
+def make_cake(flour_exists)
+  if flour_exists
+    return :cake
+  else
+    return 0
+  end
+end
+make_cake(true)
+#=> cake
+make_cake(false)
+#=> 0
 
-words = ["Hello", "World!"]
+ def average(scores)
+   score_accumulator = 0
 
-words.include?("Hello")
-#return true 
+   scores.each do |score|
+     score_accumulator += score
+   end
+
+   score_accumulator.to_f / scores.length
+ end
