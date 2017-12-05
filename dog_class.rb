ff5f6fc768771826de758126efe81647545dac57
Leigh-ann <leighann.gant@gmail.com>
2017-12-05 17:32:45 +0000

    Robot class created

--- dog_class.rb
+++ dog_class.rb
@@ -6,4 +6,12 @@ class Dog
   def num_of_legs
     4
   end
+
+  def colour=(colour)
+    @colour = colour
+  end
+
+  def observe
+    "You see a " +@colour + " dog"
+  end
 end
---
+++ robot_class.rb
@@ -0,0 +1,12 @@
+class Robot
+  def legs=(number_of_legs)
+    @number_of_legs = number_of_legs
+  end
+
+  def add_leg
+    @number_of_legs += 1
+  end
+  def walk
+    return "I'm walking on my " + @number_of_legs.to_s + " legs!"
+  end
+end
